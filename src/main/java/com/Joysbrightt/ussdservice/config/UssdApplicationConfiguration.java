4098e7eec7518c2e3c01d2bb44ff7c4307b81bf0
Joysbrightt <oladoyinoluwatomisin11@gmail.com>
2023-06-10 11:27:42 +0100

    Revert "Revert ""added some classes and added configuration files for Redis and session"""
    This reverts commit f1b4dba179251af55a95fdb0be79bb9961b5728a.

---
+++ src/main/java/com/Joysbrightt/ussdservice/config/UssdApplicationConfiguration.java
@@ -0,0 +1,21 @@
+package com.Joysbrightt.ussdservice.config;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties
+@Data
+public class UssdApplicationConfiguration {
+    private CacheConfigurationProperties cache;
+    @Getter(value = AccessLevel.PUBLIC)
+    private class CacheConfigurationProperties{
+        private Integer port;
+        private String host;
+        private String password;
+        private String defaultTtl;
+    }
+ }
